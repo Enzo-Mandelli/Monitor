7e2425644493ea376e40e68c01d7c11ea142c245
enzo mandelli <mandelli.contato@proton.me>
2026-01-24 23:24:49 -0300

    corrigindo erro nos switch e tornando a biblioteca utilizavel no platformIO

--- src/Monitor.cpp
+++ src/Monitor.cpp
@@ -1,48 +1,48 @@
 #include <Monitor.h>
 #include <Arduino.h>
 
-String Monitor :: convertData(void* ptr, char type, String nome){
+void Monitor :: convertData(void* ptr, char type, String nome){
     bool rightType = false;
     String typeVar;
     //types i = int; b = bool; f = float; s = String; c = char
     switch (type){
-    case 'i':
+    case 'i':{
         typeVar = "int";
         int* ptrTyped = (int*)ptr;
         prepareStatement(nome, String(*ptrTyped), typeVar);
         break;
-
-    case 'b':
+    }
+    case 'b':{
         typeVar = "bool";
         bool* ptrTyped = (bool*)ptr;
         prepareStatement(nome, String(*ptrTyped), typeVar);
         break;
-    
-    case 'f':
+    }
+    case 'f':{
         typeVar = "float";
         float* ptrTyped = (float*)ptr;
         prepareStatement(nome, String(*ptrTyped), typeVar);
         break;
-    
-    case 's':
+    }
+    case 's':{
         typeVar = "String";
         String* ptrTyped = (String*)ptr;
         prepareStatement(nome, String(*ptrTyped), typeVar);
         break;
-    
-    case 'c':
+    }
+    case 'c':{
         typeVar = "char";
         char* ptrTyped = (char*)ptr;
         prepareStatement(nome, String(*ptrTyped), typeVar);
         break;
-
+    }
     default:
         break;
     }
 
 }
 
-String Monitor :: prepareStatement(String name, String value, String type){
+void Monitor :: prepareStatement(String name, String value, String type){
     //statement is formed by [names] + [values] + [types]
     String placeHolder = statement;
     name = sliceStr(statement, 0) + "," + name;
@@ -79,50 +79,61 @@ void Monitor :: addChar(String nome, char* ptr){
     convertData(ptr, 'i', nome);
 }
 
-void Monitor :: addString(String nome, String ptr){
+void Monitor :: addString(String nome, String* ptr){
     convertData(&ptr, 's', nome);
 }
 
 void Monitor :: update(){
     //data order [pointer],[data], [type];
-    if(web.checkConnection()){
-        // O '0' no final do strtoul detecta automaticamente se é decimal ou hexa (0x)
+    if(wait()){
+        if(web.checkConnection()){
+            previousTime = millis();
+            // O '0' no final do strtoul detecta automaticamente se é decimal ou hexa (0x)
 
-        uintptr_t pointer = strtoul(sliceStr(web.receiveData(), 0).c_str(), NULL, 0);
+            uintptr_t pointer = strtoul(sliceStr(web.receiveData(), 0).c_str(), NULL, 0);
 
-        String type = sliceStr(web.receiveData(), 2);// procura o tipo de dado
-        switch (type.charAt(0)){// o switch precisa de um char, então seleciona o primeiro caracter que e o unico necessario
-            case 'i':
-                int* ptrTyped = (int*)pointer;
-                *ptrTyped = (sliceStr(web.receiveData(), 1)).toInt();
-                break;
-
-            case 'b':
-                bool* ptrTyped = (bool*)pointer;
-                if(sliceStr(web.receiveData(), 1) == "t"){
-                    *ptrTyped = 1;
-                }else{
-                    *ptrTyped = 0;
+            String type = sliceStr(web.receiveData(), 2);// procura o tipo de dado
+            switch (type.charAt(0)){// o switch precisa de um char, então seleciona o primeiro caracter que e o unico necessario
+                case 'i':{
+                    int* ptrTyped = (int*)pointer;
+                    *ptrTyped = (sliceStr(web.receiveData(), 1)).toInt();
+                    break;
                 }
-                break;
+                case 'b':{
+                    bool* ptrTyped = (bool*)pointer;
+                    if(sliceStr(web.receiveData(), 1) == "t"){
+                        *ptrTyped = 1;
+                    }else{
+                        *ptrTyped = 0;
+                    }
+                    break;
+                }
+                case 'f':{
+                    float* ptrTyped = (float*)pointer;
+                    *ptrTyped = (sliceStr(web.receiveData(), 1)).toFloat();
+                    break;
+                }
+                case 's':{
+                    String* ptrTyped = (String*)pointer; // Ponteiro para o OBJETO String
+                    //*ptrTyped = String(sliceStr(web.receiveData(), 1));
+                    break;
+                }
+                case 'c':{
+                    char* ptrTyped = (char*)pointer;
+                    *ptrTyped = (sliceStr(web.receiveData(), 1)).charAt(0);
+                    break;
+                }
+                default:
+                    break;
+            }
             
-            case 'f':
-                float* ptrTyped = (float*)pointer;
-                *ptrTyped = (sliceStr(web.receiveData(), 1)).toFloat();
-                break;
-            
-            case 's':
-                String* ptrTyped = (String*)pointer; // Ponteiro para o OBJETO String
-                //*ptrTyped = String(sliceStr(web.receiveData(), 1));
-                break;
-            
-            case 'c':
-                char* ptrTyped = (char*)pointer;
-                *ptrTyped = (sliceStr(web.receiveData(), 1)).charAt(0);
-                break;
-            default:
-                break;
         }
-        
     }
+}
+
+bool Monitor :: wait(){
+    actualTime = millis(); // Pega o tempo agora
+    // Verifica se o tempo decorrido é maior ou igual ao intervalo
+    if (millis() - previousTime >= web.delayTime) return true;
+    return false; // Ainda não deu o tempo
 }
--- src/Monitor.h
+++ src/Monitor.h
@@ -6,16 +6,18 @@ class Monitor{
         Monitor(){
             web.getConection();
         }
-        int contVar = 0;
+        unsigned long actualTime = 0;
+        unsigned long previousTime = 0;
         void update();
         void addInt(String nome, int* ptr);
         void addFloat(String nome, float* ptr);
         void addBool(String nome, bool* ptr);
-        void addString(String nome, String ptr);
+        void addString(String nome, String* ptr);
         void addChar(String nome, char* ptr);
-        String convertData(void* ptr, char type, String nome);
+        bool wait(); //vai esperar o delayTime
+        void convertData(void* ptr, char type, String nome);
+        void prepareStatement(String name, String value, String type);
         String sliceStr(String txt, int index);
-        String prepareStatement(String name, String value, String type);
         String statement = "";
         //statemente order
         //[nameVar1, nameVar2, nameVar3], [value1, value2, value3], [tipo1, tipo2, tipo3], [pointer1, pointer2, pointer3]
